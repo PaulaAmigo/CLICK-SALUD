aac55b19b1dc00f1e3608f98ff63c11177ea078c
paulaAmigo <paula.amigo@hotmail.cl>
2023-03-13 10:01:10 -0300

    pag corporativa

---
+++ click_salud/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import CorpWeb from "./components/CorpWeb";
+
+function App() {
+  return (
+    <div class="App">
+      <CorpWeb></CorpWeb>
+    </div>
+  );
+}
+
+export default App;
---
+++ click_salud/src/components/CorpWeb.jsx
@@ -0,0 +1,50 @@
+import "../App.css";
+import React, {useState} from "react";
+
+
+
+export default function CorpWeb() {
+
+    const [isOpen, setIsOpen] = useState(false)
+  return (
+    <div className='CorpWeb'>
+      <div className="logo"> <img src="logo.JPG"></img>Click Salud</div>
+      <div className={`items ${isOpen && "open"}`}>
+          <a href="#">Inicio</a>
+          <a href="#">Quienes Somos</a>
+          <a href="#">Servicios</a>
+          <a href="#">Contacto</a>
+          <a href="#">Agenda tu hora</a>
+          
+      </div>
+      <div className={`toggle ${isOpen && "open"}`} onClick={ () => setIsOpen(!isOpen)} >
+      <span></span>
+      <span></span>
+      <span></span>
+      </div>
+      <div className="inicio">
+          <h1>Bienvenidos a Click Salud </h1>
+          <p>Donde tienes salud a un click</p>
+      </div>
+      <div className="about">
+          <h1>Quienes somos</h1>
+          <p>Somos una empresa...</p>
+          <h2>Misión</h2>
+          <p>....</p>
+          <h2>Vissión</h2>
+          <p>...</p>
+      </div>
+      <div className="servicios">
+          <h1>Servicios</h1>
+          <h3>Telemedicina</h3>
+          <p>...</p>
+          <h3>Atención domiciliaria</h3>
+      </div>
+      <div className="contacto">
+          <h1>Contacto</h1>
+          <p>Nuestros canales de contacto son......</p>
+      </div>
+      
+    </div>
+  )
+}
